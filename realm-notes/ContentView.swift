ea8f2c16c6b20d90ae723268cc2c6f55c6fa1a05
irfanizudin <irfanizzudyn@gmail.com>
2023-01-14 12:04:37 +0700

    Initial Commit

---
+++ realm-notes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  realm-notes
+//
+//  Created by Irfan Izudin on 14/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ realm-notes/realm_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  realm_notesApp.swift
+//  realm-notes
+//
+//  Created by Irfan Izudin on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct realm_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
